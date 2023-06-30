bc72a501ec933f902cf3cbb93f87450635d4f81e
Nicky <raccoonutau@gmail.com>
2023-06-30 13:48:30 -0500

    Update pairandcompare.js
    Fixed semantic issue

--- pairandcompare.js
+++ pairandcompare.js
@@ -1,12 +1,12 @@
 console.log("Hi! This is the Pair and Compare Exercise.")
 //Testing the script
-let param1A = 0;
-let param1B = false;
+let param1A = "dog";
+let param1B = "dawg";
 //Declares your first 2 values
 let param2A = "horse";
-let param2B = "horse";
+let param2B = 5;
 //declares your second pair of values
-if ((param1A && param1B) || (param2A && param2B)) {
+if ((param1A == param1B) || (param2A == param2B)) {
     console.log(true);
 } else {
     console.log(false);
